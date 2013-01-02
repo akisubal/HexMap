4052660d0d8b96484792b50e6fde61a5cc3807b8
立石 昂裕 <akisubal@tatsuishi-no-MacBook-Air.local>
2013-01-02 20:30:50 +0900

    Initial Commit

---
+++ Hex/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  Hex
+//
+//  Created by 立石 昂裕 on 2013/01/02.
+//  Copyright (c) 2013年 立石 昂裕. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
